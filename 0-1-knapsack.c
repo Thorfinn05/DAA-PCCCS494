d644e9f1e743c4478b8b0d3ef4ed47335edd1798
Rudranil Das <rudranild04@gmail.com>
2025-06-18 11:48:51 +0530

    Refactored loop indices in prac function to start from 0 for consistency

--- 0-1-knapsack.c
+++ 0-1-knapsack.c
@@ -64,8 +64,8 @@ int max(int a, int b){
 
 void prac(int prof[MAX], int wt[MAX], int n, int W){
     int dp[MAX+1][MAX+1];
-    for(int i=1; i<=n; i++){
-        for(int w=1; w<=W; w++){
+    for(int i=0; i<=n; i++){
+        for(int w=0; w<=W; w++){
             if(i==0||w==0){
                 dp[i][w] = 0;
             }
